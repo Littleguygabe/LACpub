4636a50e82995ccc2a8f1b77eec164d640fbefbb
Gabriel Bridger <gbridger775@gmail.com>
2026-03-26 17:05:17 +0000

    Add Pumping Lemma and Regular Expressions Implementation
    - Introduced the Pumping Lemma theorem for regular languages in `Pumping.lean`.
    - Implemented regular expressions and their conversion to NFAs in `PDA.lean`.
    - Added definitions for context-free grammars and a PDA for bracket-matched words in `ex5.lean`.
    - Enhanced the `L10.lean` lecture notes with detailed explanations of regular languages and the Pumping Lemma.

--- Lectures/L10.lean
+++ Lectures/L10.lean
@@ -10,96 +10,187 @@ variable {Sigma : Type}[Alphabet Sigma]
 
 def REG : Set (Lang Sigma)
 := {lang | ∃ A : DFA Sigma, Dfa.L A = lang}
+
+-- The halting problem
+  -- says something about reuglar languages
 /-
 What is a regular language?
-A language that can be represented by a DFA (or NFA or RE).
-Given a regular language, we may not know the DFA, but we know
-that it has some number n of states.
-If we have an accepted word of length greater than n, then
-it must traverse some nonempty loop in the DFA.
-But that must mean that the word traversing this loop multiple
-times, is also in the language.
-The first loop must occur in the first n steps.
-So the repeated substring is also in the first n characters.
-So, for all words longer than n,
-There exists a nonempty subword within the first n characters
-such that the word obtained by repeated or omitting this
-subword is also accepted.
+  > A language that can be represented by a DFA (or NFA or RE)
+
+Give a regular language we may not know the DFA,
+but we know it has some number `n` of states
+
+so if we have any word of length > `n` it must visit atleast one state more than once
+  > ie it must traverse some non-empty loop in the DFA
+
+So that must mean that the word doing this loop multiple times is also in the language
+
+So when does this loop occur?
+  > The first loop must occur in the first `n` steps
+  > So the repeated substring is also in the first `n` characters
+
+So for all words longer than `n`
+there is some non-empty sub word in the first `n` characters
+such that the word obtained by repeating or omitting this subword is also accepted
+ie
+  > if a-ab-a is accepted with `ab` as our subword
+  > the word a-a obtained from omitting `ab` is also accepted (as we just don't loop)
+  > the word a-ab-ab-a obtained from repeating the `ab` subword is also accepted (as we just loop more times)
+
+    > all we're doing is changing the number of loops we
 -/
+-- ! So by formalising this we get the **Pumping Lemma**
+
 theorem pumping_lemma : ∀ L₁ : Lang Sigma,
-  L₁ ∈ REG →
-    (∃ n : ℕ,
-    ∀ w : Word Sigma,
-    w.length ≥ n ∧ w ∈ L₁ →
-      ∃ x y z : Word Sigma,
-      w = x ++ y ++ z ∧
-      (x ++ y).length ≤ n ∧
-      y.length ≥ 1 ∧
-      ∀ m : ℕ, x ++ y^m ++ z ∈ L₁
+  L₁ ∈ REG → -- if the language is regular
+    (∃ n : ℕ, -- there exists some number n (number of states in DFA)
+    ∀ w : Word Sigma, -- such that for all words
+    w.length ≥ n ∧ w ∈ L₁ → -- where the word length is greater than the number of states
+                            -- and the word is in the the language
+      ∃ x y z : Word Sigma, -- there exists words x y z
+      w = x ++ y ++ z ∧ -- such that the word w is the concatenation of x y z
+      (x ++ y).length ≤ n ∧ -- and the length of the concatenation of x y
+                            -- ≤ the number of states in our DFA
+      y.length ≥ 1 ∧ -- and the length of y (the subword we loop) ≥ 1
+      ∀ m : ℕ, x ++ y^m ++ z ∈ L₁ -- and for all natural numbers m, if we repeat y m times
+                                  -- the word created is still in our language
     ) := by sorry
-/-
-The pumping lemma is used to show that a language is not regular:
-1. Assume L is regular
-2. Assume we have a pumping length n
-3. We choose w such that w.length ≥ n and w ∈ L
-4. Then we show that for _any_ split xyz = w with |xy| ≤ n and |y| ≥ 1,
-  There exists some repetition of y's such that xy^mz is not in the language
 
-Example: L = { a^n | n is prime }
-1. Assume L is regular.
-2. That means we have a pumping length n
-3. Let w = a^m where m is the first prime greater than n
-4. Let x y z be such that xy = a^p for p ≤ n and y = a^q for 1 ≤ q ≤ p
-Now consider x ++ y^s ++ z. What is this word?
-xy^sz = x ++ y^s ++ z
-x = a^{p-q}
-z = a^{m-p}
-y^s = a^{s*q}
-Thus, xy^sz = a^{(p-q)+(s*q)+(m-p)} = a^{m+(s-1)*q}
--- (p-q)+(s*q)+(m-p) = p-q+m-p+s*q = -q+m+s*q = m+s*q-q = m+(s-1)*q
-If we pick s = m+1
-then x++y^s++z = a^{m+(m+1-1)*q} = a^{m+m*q} = a^{m(1+q)}
-That must must that x++y^s++z is not in L
-That contradicts the pumping lemma
-That must mean that L is not regular.
+-- ! This is then used to show a language is not regular
+-- 1. Assume the language is regular
+-- 2. Then prove that the pumping lemma does not hold
+/- ? Then how do we prive the pumping lemma does not hold?
+by proving the negation of `pumping_lemma`
+
+1. Assume we have a pumping length `n`
+2. We choose a w such that:
+  w.length ≥ n
+  w ∈ L
+
+3. Then we show that for any split xyz = w with
+  (x++y).length ≤ n
+  y.length ≥ 1
+There exists some repeitions of y's such that xy^mz is not in the language
 -/
-end pumping
-/-
-Context-free grammars (CFG)
-A CFG consists of
- - Nonterminal characters
- - Terminal characters
- - A starting character
- - Derivation rules.
 
-Example : simple arithmetic formulas like
-a + (a*a)
+/- ! Example L = {a^n | n is prime}
+1. Assume L is regular
 
-Let's create a CFG for this language
-Nonterminals are E, T, F
-Starting symbol: S = E
-Terminals here are: a, +, (, *, )
+2. That means we have a pumping length n
+
+3. Let w = a^m
+  where m is the first prime greater than n
+
+4. Let x y z be such that
+  xy = a^p for p ≤ n
+  y = a^q for 1 ≤ q ≤ p
+
+Now consider x ++ y^s ++ z. What is this word?
+  xy^sz = x ++ y^s ++ z
+    x = a^{p - q}
+    z = a^{m - p}
+    y^s = a^{s × q}
+  so
+  xy^sz = a^{p - q} ++ a^{s × q} ++ a^{m - p}
+        = a^{p - q} × a^{s × q} × a^{m - p}
+        = a^{p - q + s × q + m - p}
+        = a^{- q + s × q + m}
+        = a^{(s - 1) × q + m}
+
+If we pick s = m+1
+then x ++ y^s ++ z = a^{(m + 1 - 1) × q + m}
+                   = a^{m (1 + q)}
+
+hence not prime as we can take a factor of m or 1 + q out
+
+That must mean that x ++ y^s ++ z is not in L
+That contradicts the pumping lemma
+That must mean that L is not regular
+-/
+
+-- ! Moving onto Context Free Grammars (CFG)
+
+/- ? First what is the definition of a regular expression
+
+Components, either:
+  empty word - ε
+  or empty language - ∅
+  or language that contains a single character - {[x]}
+  or regular expression + regular expression - ∨
+  or regular expression ++ regular expression - ∧
+  or regular expression * - repeating RE
+  or (regular expression) - putting a RE in brackets
+-/
+
+/- ? So how is it created?
+Start with a character regular expression, ie `S`
+  Called a non-terminal in CFG's
+
+Which is then replaced by any of the components of a regular expression
+-/
+
+/- ? What are the components of a context free grammar (CFG)
+- Non-Terminal Characters
+    Only 1 in regular expr (RE)
+- Terminal Characters
+    Every component of a regular expression (shown above)
+- A Starting Character
+    Only non-terminal we have (RE)
+- Derivation Rules
+    How we translate RE (Non-terminal starting character) into a structure defined by the language
+-/
+
+/- ! Example of creating a CFG for a language
+Language of creating simple arithmetic formulas like
+  a + (a*a)
+
+Non-terminals are
+  1. E - one for expressions
+  2. T - one for terms
+  3. F - one for factors
+
+Starting symbol: S = E (expressions)
+Terminals here are: a, +, (, ), *
+
 Derivation rules:
-E => T
-E => E + T
-T => F
-T => T*F
-F => a
-F => (E)
+  E => T
+    An Expression can become a Term
 
-How would this generate a formula?
-E.g. a + (a*a)
-This generated by
-E => E + T => E + F
-=> E + (E) => E + (T)
-=> E + (T*F) => E + (T*a)
-=> E + (F*a) => E + (a*a)
-=> T + (a*a) => F + (a*a)
-=> a + (a*a)
+  E => E + T
+    An Expression can be an Expression plus a Term
 
-Language of a CFG is all those
-words over the terminal characters
-such that there exists a chain
-of derivations from the starting NT
-to that word.
+  T => F
+    A Term can be a Factor
+
+  T => T * F
+    A Term can be a Term multiplied by a Factor
+
+  F => a
+    A Factor can be a Number
+
+  F => (E)
+    A Factor can be an Expression within brackets
+-/
+
+/- ? How would this generate a formula?
+Eg a + (a*a)
+
+a + (a*a) = E
+=> E + T - E is E + T
+=> T + F - E is T, T is F
+=> F + (E) - T is F, F is (E)
+=> a + (T) - F is a, E is T
+=> a + (T * F) - T is T * F
+=> a + (F * a) - T is F, F is a
+=> a + (a * a) - F is a
+
+which then gives us our final expression
+
+So its like defining an algebraic data type for a DSL in haskell
+
+so the language of a CFG is
+all those words oer the terminal characters,
+such that there exists a chain of derivations
+from the starting non-terminal to that word
+
 -/
